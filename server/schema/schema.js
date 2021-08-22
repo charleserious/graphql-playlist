83a7fd85032861a099b3133574335655b0830e53
charleserious <wujinjing25@gmail.com>
2021-08-22 22:29:21 +0800

    feat: type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,9 +10,9 @@ const {
 
 // dummy data
 const books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
 ];
 
 const authors = [
@@ -27,6 +27,13 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve: (parent, args) => {
+        console.log(parent, args);
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
   }),
 });
 
@@ -46,7 +53,6 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        console.log(typeof args.id);
         return _.find(books, { id: args.id });
       },
     },
