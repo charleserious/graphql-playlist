2915b9baeebb3edf1f2c6730f1ff5fda9a2ad04e
charleserious <wujinjing25@gmail.com>
2021-08-24 21:14:59 +0800

    book list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import { BookList } from "./components/BookList";
 function App() {
   return (
     <div className="App">
       <h1>Reading List</h1>
+      <BookList></BookList>
     </div>
   );
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,9 @@
+export const BookList = (props) => {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>Book name</li>
+      </ul>
+    </div>
+  );
+};
