c287fdfc698b8588ad9422f83033813fc9720c05
charleserious <wujinjing25@gmail.com>
2021-08-26 22:48:46 +0800

    update component state

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,28 +1,46 @@
 import { useQuery } from "@apollo/client";
 import { AUTHORS } from "../queries/queries";
+import { useState } from "react";
 
 export const AddBook = (props) => {
   const { loading, error, data } = useQuery(AUTHORS);
+  const [book, setBook] = useState({ name: "", genre: "", authorId: "" });
+  const { name, genre } = book;
+
+  const onChangeHandler = ({ target }) => {
+    const { id, value } = target;
+    setBook({ ...book, [id]: value });
+  };
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    console.log(book);
+  };
 
   if (loading) return <div>Loading Authors...</div>;
   if (error) return <div>Error :(</div>;
 
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={onSubmitForm}>
       <div className="field">
-        <label htmlFor="bookName">Book name:</label>
-        <input id="bookName" type="text" />
+        <label htmlFor="name">Book name:</label>
+        <input id="name" type="text" value={name} onChange={onChangeHandler} />
       </div>
 
       <div className="field">
         <label htmlFor="genre">Genre:</label>
-        <input id="genre" type="text" />
+        <input
+          id="genre"
+          type="text"
+          value={genre}
+          onChange={onChangeHandler}
+        />
       </div>
 
       <div className="field">
-        <label htmlFor="author">Author:</label>
-        <select id="author">
-          <option>Select author</option>
+        <label htmlFor="authorId">Author:</label>
+        <select id="authorId" onChange={onChangeHandler}>
+          <option value={""}>Select author</option>
           {data.authors.map(({ name, id }) => (
             <option key={id} value={id}>
               {name}
